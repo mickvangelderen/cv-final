672ce719a6fb7e83fdfc5065effafd695a4eb627
Mick van Gelderen <mickvangelderen@gmail.com>
2015-07-03 17:37:46 +0200

    Added some controls yeah

--- fragment.glsl
+++ fragment.glsl
@@ -3,35 +3,37 @@ precision mediump float;
 uniform sampler2D tex;
 uniform vec2 texSize;
 uniform float kernel[25];
+uniform vec3 bias;
 
 varying vec2 texCoords;
 
 void main() {
 	vec2 d = vec2(1.0)/texSize;
-	gl_FragColor = vec4(0.5) +
-		kernel[ 0]*texture2D(tex, texCoords + d*vec2(-2.0,  2.0)) +
-		kernel[ 1]*texture2D(tex, texCoords + d*vec2(-1.0,  2.0)) +
-		kernel[ 2]*texture2D(tex, texCoords + d*vec2( 0.0,  2.0)) +
-		kernel[ 3]*texture2D(tex, texCoords + d*vec2( 1.0,  2.0)) +
-		kernel[ 4]*texture2D(tex, texCoords + d*vec2( 2.0,  2.0)) +
-		kernel[ 5]*texture2D(tex, texCoords + d*vec2(-2.0,  1.0)) +
-		kernel[ 6]*texture2D(tex, texCoords + d*vec2(-1.0,  1.0)) +
-		kernel[ 7]*texture2D(tex, texCoords + d*vec2( 0.0,  1.0)) +
-		kernel[ 8]*texture2D(tex, texCoords + d*vec2( 1.0,  1.0)) +
-		kernel[ 9]*texture2D(tex, texCoords + d*vec2( 2.0,  1.0)) +
-		kernel[10]*texture2D(tex, texCoords + d*vec2(-2.0,  0.0)) +
-		kernel[11]*texture2D(tex, texCoords + d*vec2(-1.0,  0.0)) +
-		kernel[12]*texture2D(tex, texCoords + d*vec2( 0.0,  0.0)) +
-		kernel[13]*texture2D(tex, texCoords + d*vec2( 1.0,  0.0)) +
-		kernel[14]*texture2D(tex, texCoords + d*vec2( 2.0,  0.0)) +
-		kernel[15]*texture2D(tex, texCoords + d*vec2(-2.0, -1.0)) +
-		kernel[16]*texture2D(tex, texCoords + d*vec2(-1.0, -1.0)) +
-		kernel[17]*texture2D(tex, texCoords + d*vec2( 0.0, -1.0)) +
-		kernel[18]*texture2D(tex, texCoords + d*vec2( 1.0, -1.0)) +
-		kernel[19]*texture2D(tex, texCoords + d*vec2( 2.0, -1.0)) +
-		kernel[20]*texture2D(tex, texCoords + d*vec2(-2.0, -2.0)) +
-		kernel[21]*texture2D(tex, texCoords + d*vec2(-1.0, -2.0)) +
-		kernel[22]*texture2D(tex, texCoords + d*vec2( 0.0, -2.0)) +
-		kernel[23]*texture2D(tex, texCoords + d*vec2( 1.0, -2.0)) +
-		kernel[24]*texture2D(tex, texCoords + d*vec2( 2.0, -2.0));
+	gl_FragColor = vec4(bias, 1.0) + vec4(
+		kernel[ 0]*vec3(texture2D(tex, texCoords + d*vec2(-2.0,  2.0))) +
+		kernel[ 1]*vec3(texture2D(tex, texCoords + d*vec2(-1.0,  2.0))) +
+		kernel[ 2]*vec3(texture2D(tex, texCoords + d*vec2( 0.0,  2.0))) +
+		kernel[ 3]*vec3(texture2D(tex, texCoords + d*vec2( 1.0,  2.0))) +
+		kernel[ 4]*vec3(texture2D(tex, texCoords + d*vec2( 2.0,  2.0))) +
+		kernel[ 5]*vec3(texture2D(tex, texCoords + d*vec2(-2.0,  1.0))) +
+		kernel[ 6]*vec3(texture2D(tex, texCoords + d*vec2(-1.0,  1.0))) +
+		kernel[ 7]*vec3(texture2D(tex, texCoords + d*vec2( 0.0,  1.0))) +
+		kernel[ 8]*vec3(texture2D(tex, texCoords + d*vec2( 1.0,  1.0))) +
+		kernel[ 9]*vec3(texture2D(tex, texCoords + d*vec2( 2.0,  1.0))) +
+		kernel[10]*vec3(texture2D(tex, texCoords + d*vec2(-2.0,  0.0))) +
+		kernel[11]*vec3(texture2D(tex, texCoords + d*vec2(-1.0,  0.0))) +
+		kernel[12]*vec3(texture2D(tex, texCoords + d*vec2( 0.0,  0.0))) +
+		kernel[13]*vec3(texture2D(tex, texCoords + d*vec2( 1.0,  0.0))) +
+		kernel[14]*vec3(texture2D(tex, texCoords + d*vec2( 2.0,  0.0))) +
+		kernel[15]*vec3(texture2D(tex, texCoords + d*vec2(-2.0, -1.0))) +
+		kernel[16]*vec3(texture2D(tex, texCoords + d*vec2(-1.0, -1.0))) +
+		kernel[17]*vec3(texture2D(tex, texCoords + d*vec2( 0.0, -1.0))) +
+		kernel[18]*vec3(texture2D(tex, texCoords + d*vec2( 1.0, -1.0))) +
+		kernel[19]*vec3(texture2D(tex, texCoords + d*vec2( 2.0, -1.0))) +
+		kernel[20]*vec3(texture2D(tex, texCoords + d*vec2(-2.0, -2.0))) +
+		kernel[21]*vec3(texture2D(tex, texCoords + d*vec2(-1.0, -2.0))) +
+		kernel[22]*vec3(texture2D(tex, texCoords + d*vec2( 0.0, -2.0))) +
+		kernel[23]*vec3(texture2D(tex, texCoords + d*vec2( 1.0, -2.0))) +
+		kernel[24]*vec3(texture2D(tex, texCoords + d*vec2( 2.0, -2.0))),
+	1.0);
 }
